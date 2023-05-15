3c66f1743468722101757b00d52b6f324b96b129
nikhil kumar <nikhilkumar@nikhils-Air.hsd1.ga.comcast.net>
2023-05-15 14:36:49 -0400

    updated

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.printf("Hello world");
-        System.out.printf("Hello world tarung");
+        System.out.printf("Hello world NIKHIL");
 
     }
 }
